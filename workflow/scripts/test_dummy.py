69a9902824a75a4b042863e5d503ba48ba845cff
jonca79 <jonas.almlof@igp.uu.se>
2021-11-02 15:15:06 +0100

    initial template build from hydra-genetics/tools, version 0.0.6

---
+++ workflow/scripts/test_dummy.py
@@ -0,0 +1,12 @@
+# vim: syntax=python tabstop=4 expandtab
+# coding: utf-8
+
+__author__ = "Jonas A"
+__copyright__ = "Copyright 2021, Jonas A"
+__email__ = "jonas.almlof@igp.uu.se"
+__license__ = "GPL-3"
+
+
+def test_dummy():
+    from dummy import dummy
+    assert dummy() == 1
